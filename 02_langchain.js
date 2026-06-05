5831aac902e3a87f0e793380c36cf414339eae07
JTaeHyeon <ghgh036034@gmail.com>
2026-06-05 10:37:01 +0900

    feat: LangChain 스크립트 추가 및 패키지 설정 업데이트

---
+++ 02_langchain.js
@@ -0,0 +1,25 @@
+//환경변수 불러오기
+const dotenv = require("dotenv");
+dotenv.config();
+
+//의존성
+const express = require("express");
+const {} = require("@langchain/core");
+
+// 서버 세팅
+const PORT = process.env.PORT ?? 3000;
+const app = express();
+
+//middleware
+app.use(express.json());
+
+//routher, endpoint...
+app.post("/chat", async (req, res) => {
+  console.log(...req.body);
+  res.json(...req.body);
+});
+
+//listener
+app.listen(PORT, () => {
+  console.log(`${PORT}에서 Listen 중`);
+});
